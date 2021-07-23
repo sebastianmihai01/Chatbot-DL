c12905fc99a08185445a93c9326d27c9ee2025c6
Sebi <mihaisebastian01@gmail.com>
2021-07-23 16:47:00 +0300

    reformat

--- NN/model.py
+++ NN/model.py
@@ -28,6 +28,6 @@ words=[]
 classes = []
 documents = []
 ignore_words = ['?', '!']
-data_file = open('intents.json').read()
+data_file = open('../intents.json').read()
 intents = json.loads(data_file)
 
