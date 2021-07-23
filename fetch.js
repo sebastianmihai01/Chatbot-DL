9ff88828c5961adda6fd11d112866f1fb2054f0a
Sebi <mihaisebastian01@gmail.com>
2021-07-23 16:49:02 +0300

    fetch api

---
+++ fetch.js
@@ -0,0 +1,17 @@
+//api link
+const url = '';
+
+let data = {
+  name: 'Sara'
+}
+
+var request = new Request(url, {
+    method: 'POST',
+    body: data,
+    headers: new Headers()
+});
+
+fetch(request)
+.then(function() {
+    // Handle response we get from the API
+})
